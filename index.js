16531ffc4ed46ae19636dfa9460235dbe2b313d3
Daniel Sinclair <d@niel.nyc>
2021-07-23 03:04:41 -0400

    write schema to file

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
-const core = require('@actions/core');
-const wait = require('./wait');
 
+const core = require('@actions/core')
+const exec = require('@actions/exec')
+const fs = require('fs')
 
-// most @actions toolkit packages have async methods
+const rover = async (args = []) => {
+  let schema = ""
+  const stdout = (data) => schema += data.toString()
+  const listeners = { stdout }
+  const options = { listeners }
+  await exec.exec("/root/.rover/bin/rover", args, options)
+  return schema
+}
+
+const getInput = () => {
+  const graph = core.getInput('graph')
+  const variant = core.getInput('variant')
+  const federated = core.getInput('federated')
+  const subgraph = core.getInput('subgraph')
+  if (federated && !subgraph) throw new Error('federated graph requires subgraph input')
+  const path = federated ? `./${subgraph}.graphql` : `./graph.graphql`
+  return { graph, variant, federated, subgraph, path }
+}
+
+const setOutput = (path, schema) => {
+  fs.writeFileSync(path, schema)
+  core.setOutput('path', path)
+  const encoded = Buffer.from(schema).toString('base64')
+  core.setOutput('schema', encoded)
+}
+
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    const { graph, variant, federated, subgraph, path } = getInput()
+    const name = federated ? ['--name', subgraph] : []
+    const args = [
+      federated ? 'subgraph' : 'graph',
+      'fetch',
+      `${graph}@${variant}`,
+      ...name
+    ]
+    const schema = await rover(args)
+    setOutput(path, schema)
   } catch (error) {
-    core.setFailed(error.message);
+    console.error(error)
+    core.setFailed(error.message)
   }
 }
 
-run();
+run()
